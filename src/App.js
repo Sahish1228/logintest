59e84aa5f090d9dc38c9a17b2a1b87b36df84b3d
Ashish Kumar Yadav <sahishkumar09@gmail.com>
2021-07-31 13:34:57 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1> <strong> LOGIN FORM</strong></h1>
+      <input type="email" placeholder="Enter your login details" value="yadav"/>
+      <br/>
+      <br/>
+      <input type="password" placeholder="Enter your password" value="ashish"/>
+    </div>
+  );
+}
+
+export default App;
